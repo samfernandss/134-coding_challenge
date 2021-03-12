ecc932db79704911977d56f8eb670f261cd101c7
Samara Fernandes <samwallflowergirl@gmail.com>
2021-03-12 16:40:14 -0300

    mudança na mensagem e construção da primeira etapa do desafio

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 
 const poll = {
     question: "What is your favourite programming language?",
-    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+    options: ["1: JavaScript", "2: Python", "3: Rust", "4: C++"],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
     registerNewAnswer() {
         const message = `${this.question}\n ${this.options.join('\n')}`;
-        const answer = Number(prompt(message));
+        const answer = Number(prompt(message))-1;
         console.log(answer);
+        if (answer >= 0 && answer < 4){
+            this.answers[answer]++;
+            console.log(this.answers);
+        }
+        else {
+            alert('Resposta inválida');
+        }
     }
 }
 
-poll.registerNewAnswer();
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer);
+console.log(poll.registerNewAnswer());
